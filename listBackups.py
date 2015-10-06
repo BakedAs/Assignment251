20395ac2c0ccd21da51144ce9ed2761e9d1cb137
Josh Baker <joshua.baker02@gmail.com>
2015-10-06 16:51:51 +1300

    Created listbackups function

--- listBackups.py
+++ listBackups.py
@@ -1,4 +1,12 @@
 __author__ = 'JoshBaker'
+import os, os.path, utilities
 
 def list(directory, pattern = None):
-    return None
+    index = utilities.loadIndex(directory)
+    pattern = pattern.lower()
+    for key in index:
+        keyLower = key.lower()
+        if pattern == None:
+            print key
+        elif pattern in keyLower:
+            print key
