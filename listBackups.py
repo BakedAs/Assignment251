2245a4f41604b1d503b370b787cd2f40d2360e59
Josh Baker <joshua.baker02@gmail.com>
2015-10-08 13:36:44 +1300

    Added 'no matches found' to list function when pattern doesnt match

--- listBackups.py
+++ listBackups.py
@@ -5,10 +5,14 @@ def list(directory, pattern = None):
     index = utilities.loadIndex(directory)
     if pattern != None:
         pattern = pattern.lower()
+        switch = True
         for key in index:
             keyLower = key.lower()
             if pattern in keyLower:
                 print key
+                switch = False
+        if switch == True:
+            print "No matches found"
     else:
         for key in index:
             print key
