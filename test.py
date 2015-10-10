27f482cdb98b0a49d5e711cd48fa6d3c7a8778a8
Josh Baker <joshua.baker02@gmail.com>
2015-10-10 20:54:24 +1300

    completed test function

--- test.py
+++ test.py
@@ -5,11 +5,21 @@ def test(directory):
     index = utilities.loadIndex(directory)
     objectFilePath = os.path.join(directory, 'objects')
     everythingIsFine = True
+    counter = 0
+    erroneousPaths = []
     for filename, hash in index.items():
         if not os.path.exists(os.path.join(objectFilePath, hash)):
+            erroneousPaths.append(filename)
             print "This object does not have a matching file: " + hash
             everythingIsFine = False
-    if everythingIsFine:
+        else:
+            counter += 1
+    if everythingIsFine and len(erroneousPaths) < 1:
         print "All good here"
+        print "The number of valid files is: " + str(counter)
+    else:
+        print "These files do not have matching objects: "
+        for fileName in erroneousPaths:
+            print fileName
 
 
