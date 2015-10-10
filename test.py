9aef5119381d40163c3a46f834854f2dd2f2b6b0
Josh Baker <joshua.baker02@gmail.com>
2015-10-10 13:48:12 +1300

    test update

--- test.py
+++ test.py
@@ -4,14 +4,12 @@ import os, os.path, utilities
 def test(directory):
     index = utilities.loadIndex(directory)
     objectFilePath = os.path.join(directory, 'objects')
-    counter = 0
+    everythingIsFine = True
     for filename, hash in index.items():
-        max = len(index.items())
-        counter += 1
         if not os.path.exists(os.path.join(objectFilePath, hash)):
             print "This object does not have a matching file: " + hash
-        elif max <= counter:
-            print "All good here"
-    # for filename, hash in index.items():
+            everythingIsFine = False
+    if everythingIsFine:
+        print "All good here"
 
 
