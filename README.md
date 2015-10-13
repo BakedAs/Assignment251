6fa3513c70962f9360fab16eb61129fe751cbde4
Josh Baker <joshua.baker02@gmail.com>
2015-10-13 16:51:24 +1300

    Updated the readme file and updated test to check if archive exists

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
-# Assignment251
-Assignment 2 159.251
+# Assignment 2 159.251
 
-**Members**<br />
+## **Members:**<br />
 Francis - 14136398<br />
 Josh - 11264379 <br />
 
-***About:***<br />
+## **About:**<br />
 
 This is a program which can protect your data by saving it into an archive directory.    
-The direcotry has been constructed in such a way as to give every file a unique signature   
-and only save the same content once, regardless of how many file names with that content exist.    
-It has been created by two software engineering students at Massey University.  
+The directory has been constructed in such a way as to give every file a unique signature
+and only save the same content once, regardless of how many file names with that content exist.
+It has been created by two software engineering students at Massey University.
+
+## **Instructions:**<br />
+To run the program, download the source and unzip it to the archive directory you wish to backup.
+Use a terminal or command line to change to the directory containing the archive e.g: cd /path/to/directory
+If you have Python as part of your path: (OSX and Linux have this built in)
+    If you are using Windows, download Python 2.X and use 'python' in front of commands for example:
+
+       python mybackup.py init
+
+   1. Run the command: ./mybackup.py init - to initialize the backup folder
+   2. The use any of the following commands depending on what you want to do:
+
+       *   ./mybackup.py &lt;command&gt; &lt;parameter&gt;
+
+       Where command is:
+
+       *    store - will backup the directory and subsequent sub-directories given as parameter
+       *    restore - will restore the the directory or file given as a parameter
+       *    list - will display all of the directories or files given as a parameter or all if no parameter given
+       *    get - will get the file specified by the parameter given and restore it into the current working directory
+       *    test - will test the validity of the backup ensuring all files are accounted for and the file names match
+       the objects created in the objects directory (no parameter required)
--- test.py
+++ test.py
@@ -2,29 +2,32 @@ __author__ = 'JoshBaker'
 import os, os.path, utilities
 
 def test(directory, logger):
-    index = utilities.loadIndex(directory)
-    objectFilePath = os.path.join(directory, 'objects')
-    everythingIsFine = True
-    counter = 0
-    erroneousPaths = []
-    try: #Easier to ask for forgiveness than permission (EAFP) Checks the files to be iterated over exist
-        for fileName in os.listdir(objectFilePath):
-            filePath = os.path.join(objectFilePath, fileName)
-            hash = utilities.createFileSignature(filePath)[2]
-            if hash != fileName:
-                logger.warn("This object's hash does not match its filename: " + str(hash)) #logs an unsuccessful run of test function
-                everythingIsFine = False
+     if (os.path.exists(directory) and os.path.isdir(directory)):
+        index = utilities.loadIndex(directory)
+        objectFilePath = os.path.join(directory, 'objects')
+        everythingIsFine = True
+        counter = 0
+        erroneousPaths = []
+        try: #Easier to ask for forgiveness than permission (EAFP) Checks the files to be iterated over exist
+            for fileName in os.listdir(objectFilePath):
+                filePath = os.path.join(objectFilePath, fileName)
+                hash = utilities.createFileSignature(filePath)[2]
+                if hash != fileName:
+                    logger.warn("This object's hash does not match its filename: " + str(hash)) #logs an unsuccessful run of test function
+                    everythingIsFine = False
 
-        for filename, hash in index.items():
-            if not os.path.exists(os.path.join(objectFilePath, hash)):
-                erroneousPaths.append(filename)
-                logger.warn("This file could not be found in the objects directory: " + hash) #logs an unsuccessful run of test function
-                everythingIsFine = False
-            else:
-                counter += 1
-    except OSError: #throws an error if the files do not exist
-        print "Error: The file does not exist!"
+            for filename, hash in index.items():
+                if not os.path.exists(os.path.join(objectFilePath, hash)):
+                    erroneousPaths.append(filename)
+                    logger.warn("This file could not be found in the objects directory: " + hash) #logs an unsuccessful run of test function
+                    everythingIsFine = False
+                else:
+                    counter += 1
+        except OSError: #throws an error if the files do not exist
+            print "Error: The file does not exist!"
 
-    if everythingIsFine and len(erroneousPaths) < 1: #Logs the successful running of test function
-        logger.info("All objects' hashes match their filename and all index entries exist as objects in the archive")
-    logger.info("The number of valid index entries is: " + str(counter))
+        if everythingIsFine and len(erroneousPaths) < 1: #Logs the successful running of test function
+            logger.info("All objects' hashes match their filename and all index entries exist as objects in the archive")
+        logger.info("The number of valid index entries is: " + str(counter))
+     else:
+         print "The backup archive has not been created! Use 'mybackup init' to initialise the directory before calling 'test'."
